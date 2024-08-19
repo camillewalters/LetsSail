de22ac024a35ea43359eb1045b34ae5e9b781fef
Kari Wu <22714241+kari1358@users.noreply.github.com>
2024-08-18 23:12:36 -0700

    Interactive Learning Icon

--- React App/letssail/src/Components/Features.js
+++ React App/letssail/src/Components/Features.js
@@ -2,7 +2,7 @@ import { Container, Box, chakra, Text, Icon, SimpleGrid, useColorModeValue, Butt
 import { MdEventAvailable, MdAssignment, MdLocalHospital, MdLibraryBooks, MdOutlineSailing } from 'react-icons/md';
 import { GiShieldEchoes, GiBookmarklet } from "react-icons/gi";
 import { FaGamepad } from "react-icons/fa";
-import { FaHandPointer } from "react-icons/fa6";
+import { FaHandPointUp } from "react-icons/fa";
 import { AiFillSafetyCertificate } from "react-icons/ai";
 
 interface IFeature {
@@ -21,7 +21,7 @@ const features: IFeature[] = [
   {
     heading: 'Interactive Learning',
       content: 'Interact with the virtual world and characters. Sustain the memory better.',
-      icon: FaHandPointer,
+      icon: FaHandPointUp,
   },
   {
     heading: 'Risk-free Practice',
